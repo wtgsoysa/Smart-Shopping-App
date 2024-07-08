71825ad6bd686006d09044c460c67a8f1cc981a2
Thanuga Soysa <thanugasoysa2003@gmail.com>
2024-07-08 15:03:47 +0530

    Complete fragment_account_option / fragment_login layout designs

--- app/src/main/java/com/example/smart_shopping/LoginRegisterActivity.kt
+++ app/src/main/java/com/example/smart_shopping/LoginRegisterActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.shopping_app.R
 import com.example.smart_shopping.ui.theme.SmartShoppingTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +19,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            SmartShoppingTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            setContentView(R.layout.activity_login_register)
+
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SmartShoppingTheme {
-        Greeting("Android")
-    }
-}
